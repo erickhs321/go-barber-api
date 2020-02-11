17f677f2687975cb540991b3df26b622145f9524
Erick <erickhenrique321@gmail.com>
2020-02-11 18:03:26 -0300

    connection with database,  create user test

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'hello world' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Erick Henrique',
+    email: 'erickhenrique321@gmail.com',
+    password_hash: '1851651',
+  });
+  return res.json(user);
 });
 
 export default routes;
