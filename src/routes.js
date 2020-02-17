c19a41638ff7e23285d44f399c8109e90a71ae73
Erick <erickhenrique321@gmail.com>
2020-02-17 18:29:42 -0300

    jsonwebtoken, check password,  session controller

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+
 import User from './app/models/User';
+import SessionController from './app/controllers/SessionController';
 
 import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
