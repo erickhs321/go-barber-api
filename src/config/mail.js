f27f5da7bf3b2355cbd55e95fb3140ee7af550ea
Erick <erickhenrique321@gmail.com>
2020-02-23 21:38:06 -0300

    email config and send

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '34b6ac96633e1b',
+    pass: '3678b46f9cdf9d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
